c9c7c77e5c012029cc53d55fdf5e71cb75754ba8
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-09-26 02:57:50 +0000

    OOP with cryptocurrency

--- index.js
+++ index.js
@@ -1,29 +1,59 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    //Have the account balance start at $0 since that makes more sense
+    this.balance = 0;
   }
 
-  commit() {
-    balance -= this.amount;
+}
+
+class Transcation {
+  constructor (amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit () {
+    this.account.balance += this.value;
   }
 
 }
 
 
+class Withdrawal extends Transcation {
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transcation {
+
+get value() {
+  return this.amount;
+}
+}
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('billybob');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+// console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+t3 = new Withdrawal(9.99, myAccount);
+t3.commit();
+// console.log('Transaction 3: ', t3);
+
+console.log('Ending Balance:', myAccount.balance);
